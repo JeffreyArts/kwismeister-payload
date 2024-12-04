0e2720f607222968107ff992ba713da4646b5a08
Jeffrey Arts <sjeffff@gmail.com>
2024-12-04 09:35:11 +0100

    Remove obsolete file

--- src/app/multiple-choice-row-label.tsx
+++
@@ -1,8 +0,0 @@
-import { RowLabelArgs } from "payload/dist/admin/components/forms/RowLabel/types"
-
-export const RowLabel = ({ data, index }: RowLabelArgs) => {
-    const emoji = data?.correct ? "✅" : "❌"
-    const customLabel = `${emoji} | ${data?.answer || "No Answer"} `
-
-    return <div>{customLabel}</div>
-}
