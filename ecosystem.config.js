6ccc64fd83e6d38b0508d02528f9b520afa0fbe2
Jeffrey Arts <sjeffff@gmail.com>
2024-12-04 09:14:38 +0100

    Config update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,26 @@
 const env = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env"
 
-import dotenv from "dotenv"
-dotenv.config({ path: env })
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require("dotenv").config({ path: env })
 
 module.exports = {
     apps: [{
-        name: "Kwis - Jeffreyarts.nl", // Fill in a name here to recoginize your project in the list of PM2 instances
+        name: "Kwismeister", // Fill in a name here to recoginize your project in the list of PM2 instances
         script: "yarn",
         args: "serve",
         env: {
-            NODE_ENV: "staging"
+            NODE_ENV: "production"
         }
     }],
 
     deploy: {
-        staging: {
+        production: {
             user: process.env.DEPLOYMENT_USER,
             host: process.env.DEPLOYMENT_HOST,
             ref: "origin/main",
             repo: "git@jeff-payload.github.com:JeffreyArts/kwismeister-payload.git",
             path: process.env.DEPLOYMENT_PATH,
-            "post-deploy": "nvm use 20 && yarn install && yarn build && pm2 reload ecosystem.config.js --env staging"
+            "post-deploy": "nvm use 20 && yarn install && yarn build && pm2 reload ecosystem.config.js --env production"
         }
     } 
 }
